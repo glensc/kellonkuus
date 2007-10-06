db4bcc716ed00fa88d967fe660952fe0fbc2a01b
Elan Ruusamäe <glen@delfi.ee>
2007-10-06 11:18:33 +0000

    - add license
    Changed files:
        kellonkuus.c -> 1.6

--- kellonkuus.c
+++ kellonkuus.c
@@ -1,12 +1,30 @@
-#include <stdio.h>
-#include <time.h>
+/*
 
+  Tell a clock by playing sound waves of hours and minutes.
+
+  Copyright (c) 2007 Elan Ruusamäe <glen@alkohol.ee>
+
+  This program is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2 of the License, or (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General
+  Public License along with this library; if not, write to the
+  Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+  Boston, MA 02111-1307, USA.
+ */
+
+ /*
+ * $Id$
+ */
 #include <windows.h>
 #include <tchar.h>
-
-//#include <mmsystem.h>
-
-//#if defined(__GNUC__) && defined(__cplusplus) && defined(UNICODE)
 
 #if defined(UNICODE)
 #define _s "%ls"
@@ -15,22 +33,23 @@
 #endif
 
 #define	BASEPATH TEXT("\\Storage Card\\My Documents")
-/*
- * http://www.rainer-keuchel.de/wince/wince-api-diffs.txt
- */
 
 #ifndef _WIN32_WCE
 #undef PlaySound
 #define PlaySound(pszSound, hmod, fdwSound) printf("Play "_s"\n", pszSound);
-//#else
-//#undef PlaySound
-//#define PlaySound(pszSound, hmod, fdwSound) MessageBox(NULL, pszSound, TEXT("Debug"), MB_OK | MB_ICONINFORMATION | MB_TOPMOST);
+#else
+#if DEBUG
+#undef PlaySound
+#define PlaySound(pszSound, hmod, fdwSound) MessageBox(NULL, pszSound, TEXT("Debug"), MB_OK | MB_ICONINFORMATION | MB_TOPMOST);
+#endif
 #endif
 
 int main() {
 	DWORD res;
 
 	// need full path to files under WinCE
+	// WinCE lacks GetCurrentDirectory()
+	// see http://www.rainer-keuchel.de/wince/wince-api-diffs.txt
 #ifdef _WIN32_WCE
 	TCHAR *cwd = BASEPATH;
 	res = _tcslen(cwd);
